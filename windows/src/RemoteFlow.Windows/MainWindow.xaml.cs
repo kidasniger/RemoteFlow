b97baf24160dcd71348da19326c58f7e0e20b9ef
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-22 16:52:17 +0100

    fix(windows): import macro models in main window

--- windows/src/RemoteFlow.Windows/MainWindow.xaml.cs
+++ windows/src/RemoteFlow.Windows/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using Microsoft.Win32;
 using RemoteFlow.Windows.Core;
 using RemoteFlow.Windows.Screen;
+using RemoteFlow.Windows.Macros;
 
 namespace RemoteFlow.Windows;
 
